9eb08eed1e7e8ef982e6c4ec947831e5a52e8b7a
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-27 20:34:36 +0300

    Add files via upload

---
+++ working with the contents of a file/working_with_the_contents_of_a_file.py
@@ -0,0 +1,18 @@
+﻿t = 'text.txt'
+
+with open(t, 'a') as text:
+    a = True
+    print('Для выхода нажмите "q"')
+    print('______________________')
+    print('\n')
+    mes_1 = ''
+    while mes_1 != 'q':
+        mes_1 = input('Ведите имя: ')
+        text.write(f'{mes_1} Вроде человек... \n')
+        if mes_1 != 'q':
+            print(f'Имя {mes_1} сохранено \n')
+        else:
+            print('Завершение программы...')
+            break
+            
+        
