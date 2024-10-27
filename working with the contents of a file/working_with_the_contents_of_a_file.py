f349963108c7bdcbeaf1fec3be77aeda39115189
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-27 21:01:05 +0300

    Update working_with_the_contents_of_a_file.py

--- working with the contents of a file/working_with_the_contents_of_a_file.py
+++ working with the contents of a file/working_with_the_contents_of_a_file.py
@@ -2,15 +2,15 @@ t = 'text.txt'
 
 with open(t, 'a') as text:
     a = True
-    print('Для выхода нажмите "q"')
+    print('Для выхода введите "q"')
     print('______________________')
     print('\n')
     mes_1 = ''
     while mes_1 != 'q':
         mes_1 = input('Ведите имя: ')
         if mes_1 != 'q':
-            text.write(f'{mes_1} Вроде человек... \n')
-            print(f'Имя {mes_1} сохранено \n')
+            text.write(f'{mes_1} ты не очень такой... \n')
+            print(f'Имя: {mes_1} сохранено... \n')
         else:
             print('Завершение программы...')
             break
