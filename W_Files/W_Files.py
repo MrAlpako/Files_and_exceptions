c19a6d0ef53a1f3bad028a3fb9a74ba86975b57a
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-27 19:17:16 +0300

    Add files via upload

---
+++ W_Files/W_Files.py
@@ -0,0 +1,18 @@
+﻿d = 'd.txt'
+
+with open(d) as d:
+    r_d = d.readlines()
+    for i in r_d:
+       print(i.strip())
+      
+print('_________________________')
+       
+spisok = ''
+for i in r_d: 
+    spisok += i
+                  
+print(spisok)
+       
+print('_________________________')
+
+    
